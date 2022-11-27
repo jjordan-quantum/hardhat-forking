58335e44c6e062e191f8e9bbf732f1773ab06a2f
jj <constant.space@gmail.com>
2022-11-26 22:02:07 -0800

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+// TODO - install dotenv
+// TODO - install ethernal plugin
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+
+  networks: {
+    chainId: 1  // this will be a env var
+  }
+
+  // TODO - ethernal options
+};
